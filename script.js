34ca1f80f56f9a84febff03d94cde599a4168f2f
SwastikaGourh <89696534+SwastikaGourh@users.noreply.github.com>
2024-05-06 19:33:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('button');
+const gift = document.getElementById('gift');
+
+button.addEventListener('click', () => {
+ gift.innerHTML = '<img src="babajikathullu.jpg" id="giftimg" alt="Gift">';
+});
